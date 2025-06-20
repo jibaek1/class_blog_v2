03ab8613fadb8fa36a51e9ab120933ba63444362
jibaek <tngus000777@gmail.com>
2025-06-20 09:42:39 +0900

    기본 패키지 변경 도메인 중심

---
+++ src/main/java/com/tenco/blog/board/BoardRequest.java
@@ -0,0 +1,9 @@
+package com.tenco.blog.board;
+
+/**
+ * 클라이언트에게 넘어온 데이터를
+ * Object로 변화해서 전달하는 DTO 역할을 담당한다
+ */
+public class BoardRequest {
+
+}
--- src/main/java/com/tenco/blog/user/UserController.java
+++ src/main/java/com/tenco/blog/user/UserController.java
@@ -1,4 +1,4 @@
-package com.tenco.blog.Controller;
+package com.tenco.blog.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
