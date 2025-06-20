d2b8e8363c83c21149d0fb9096103d846f458da3
jibaek <tngus000777@gmail.com>
2025-06-20 14:36:52 +0900

    게시글 상세 보기

--- src/main/java/com/tenco/blog/board/BoardController.java
+++ src/main/java/com/tenco/blog/board/BoardController.java
@@ -1,10 +1,69 @@
 package com.tenco.blog.board;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller // IoC 대상 - 싱글톤 패턴으로 관리 됨
 public class BoardController {
 
+    // 생성자 의존 주의 - DI 처리
+    private final BoardPersistRepository br;
+
+    // 게시글 상세 보기 (주소설계)
+    // 주소 설계 GET  : http://localhost:8080/board/detail/3
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable(name = "id") Long id, HttpServletRequest request) {
+        Board board = br.findById(id);
+        request.setAttribute("board", board);
+        // prefix: classpath:/templates/
+        // return : board/detail
+        // suffix: .mustache
+
+        // 1차 캐시 효과 - DB에 접근하지 않고 바로 영속성 컨텍스트에서 꺼낸다.
+        //br.findById(id);
+        return "board/detail";
+    }
+
+
+    // 1. index.mustache 파일을 반환 시키는 기능을 만든다.
+
+    // 주소 설계 : http://localhost:8080/ , http://localhost:8080/index
+    @GetMapping({"/","/index"})
+    public String boardList(HttpServletRequest request) {
+        List<Board> boardList = br.findAll();
+        request.setAttribute("boardList",boardList);
+        return "index";
+    }
+
+    // 게시글 작성 화면 연결 처리
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+        return "board/save-form";
+    }
+
+    // 게시글 작성 액션(수행) 처리
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO reqDTO) {
+        // HTTP 요청 본문 : title=값&content=값&username=값
+        // form MIME ( application/x-www-form-urlencoded)
+
+        // reqDTO <-- 사용자가 던진 데이터 상태가 있음
+        // DTO -- Board -- DB
+        //Board board = new Board(reqDTO.getTitle(), reqDTO.getContent(),reqDTO.getUsername());
+        Board board = reqDTO.toEntity();
+        br.save(board);
+        return "redirect:/";
+    }
 
 }
--- src/main/java/com/tenco/blog/board/BoardRequest.java
+++ src/main/java/com/tenco/blog/board/BoardRequest.java
@@ -1,9 +1,29 @@
 package com.tenco.blog.board;
 
+import lombok.Data;
+
 /**
  * 클라이언트에게 넘어온 데이터를
  * Object로 변화해서 전달하는 DTO 역할을 담당한다
  */
 public class BoardRequest {
+
+    // 정적 내부 클래스로 기능별로 DTO 관리
+    // 게시글 저장 요청 데이터
+    // BoardRequest.SaveDTO
+    @Data
+    public static class SaveDTO {
+        private String title;
+        private String content;
+        private String username;
+
+
+        // DTO에서 Entity로 변환하는 메서드 만들기
+        // 계층간 데이터 변환을 명확하게 분리 하기 위함
+        public Board toEntity() {
+            return new Board(title, content, username);
+        }
+
+    } // end of SaveDTO
 
 }
